bd07aa7719249f7418091a333fc540642db91330
krishnagp97 <krishnapathak4511@gmail.com>
2026-03-10 23:11:33 +0530

    implement multer and auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import { User } from "../models/user.model";
+dotenv.config();
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+    try {
+        const token =
+            req.cookies?.accessToken ||
+            req.header("Authorization")?.replace("Bearer", "");
+
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request");
+        }
+
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        if (!decodedToken) {
+            throw new ApiError(401, "token is invalid");
+        }
+
+        const user = await User.findOne({ _id: decodedToken._id }).select(
+            "-password -refreshToken"
+        );
+        if (!user) {
+            throw new ApiError(401, "invalid access token");
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        throw new ApiError(401, error?.message || "invalid access token");
+    }
+});
